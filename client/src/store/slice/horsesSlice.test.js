86208dc785b39b8fc2a3efbf323f59090df52a1a
Danyil <demyanyuk76@gmail.com>
2022-08-10 23:11:04 +0300

    added test

--- client/src/store/slice/horsesSlice.test.js
+++ client/src/store/slice/horsesSlice.test.js
@@ -1,19 +1,19 @@
 import store from "../store"
 import { setHorses, setStartedRace, setWinnerHorse } from "./horsesSlice"
 
-describe("Race redux state tests", () => {
+describe("test horsesSlice", () => {
     it('Correct initial state', () => {
         const state = store.getState()
         expect(state.horses).toEqual([])
-        expect(state.raceStarted).toBe(false)
         expect(state.winner).toBe(-1)
+        expect(state.raceStarted).toBe(false)
     })
 
-    it("Should change state on action dispatch", () => {
-        store.dispatch(setHorses([{name: "T1", distance: 1000}]))
-        store.dispatch(setWinnerHorse(0))
+    it("test how data change in state", () => {
+        store.dispatch(setHorses([{name: "Horse1", distance: 1000}]))
         store.dispatch(setStartedRace(true))
-        expect(store.getState().horses).toEqual([{ name: "T1", distance: 1000 }])
+        store.dispatch(setWinnerHorse(0))
+        expect(store.getState().horses).toEqual([{ name: "Horse1", distance: 1000 }])
         expect(store.getState().winner).toBe(0)
         expect(store.getState().raceStarted).toBe(true)
     })
